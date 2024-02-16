907a53ea2988087a9f27f6c9ba10a0128bfa778f
lar9482 <lar9482@utulsa.edu>
2024-02-16 15:09:00 -0600

    Completed problem 2

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -10,5 +10,21 @@ def password_mod():
     password = ''
     # Type your code here.
 
+    for i in range(0, len(word)):
+        if (word[i] == 'i'):
+            password += '1'
+        elif (word[i] == 'a'):
+            password += '@'
+        elif (word[i] == 'm'):
+            password += 'M'
+        elif (word[i] == 'B'):
+            password += '8'
+        elif (word[i] == 's'):
+            password += '$'
+        else:
+            password += word[i]
+    password += '!'
+    print(password)
+
 if __name__ == "__main__":
     password_mod()
