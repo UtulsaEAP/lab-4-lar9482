a449f2b830bc09703f1f3bce73112af4cdd0d43e
lar9482 <lar9482@utulsa.edu>
2024-02-16 15:19:18 -0600

    Completed problem 4

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -7,7 +7,14 @@ Lab Time: 2/16/2024
 
 def reverse_string():
     # YOUR CODE HERE
-    
+    inputString = input()
+    while (inputString != 'Done' and inputString != 'done' and inputString != 'd'):
+        reverse_string = ''
+        for i in range(len(inputString)-1, -1, -1):
+            reverse_string += inputString[i]
+        
+        print(reverse_string)
+        inputString = input()
 
 if __name__ == "__main__":
     reverse_string()
