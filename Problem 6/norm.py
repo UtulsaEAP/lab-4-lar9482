c0da636c11dc36e8cc2d046708eb983ac2420e09
lar9482 <lar9482@utulsa.edu>
2024-02-16 15:30:34 -0600

    Completed problem 6

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -7,6 +7,22 @@ Lab Time: 2/16/2024
 
 def norm():
     # Write your code here
+    numDataset = int(input())
+    dataset = []
+    
+    maxNum = -10000000
+    for i in range(0, numDataset):
+        newNum = float(input())
+        maxNum = max(maxNum, newNum)
+        dataset.append(newNum)
+
+    for i in range(0, numDataset):
+        normalizedNum = dataset[i] / maxNum
+        print(f'{normalizedNum:.2f}')
+
+
+    
+
 
 if __name__ == "__main__":
     norm()
